22aa1f3596298002bd94dee7a7422b00fea51ae7
Lucas Morais <lucasmorais@clubegazetadopovo.com.br>
2022-05-14 19:18:46 -0300

    teste

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ app.get('/deploy', (req, res) => {
   return res.status(200).json({message: 'Success! Servidor rodando na AWS'});
 });
 
+app.post('/deploy', (req, res) => {
+  const {name, situation} = req.body;
+
+  return res.json({name, situation})
+})
+
 app.listen(3000, () => {
   console.log('Rodando')
 })
