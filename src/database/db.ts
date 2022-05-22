187113ceaf1ce28cc0a441e64e2904c3a7bfc956
Lucas Morais <lucasmorais@clubegazetadopovo.com.br>
2022-05-22 12:08:55 -0300

    fix: env

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,22 +1,22 @@
 import { Sequelize } from 'sequelize';
 
-// export const db = new Sequelize(
-//   'gaspar',
-//   'admin',
-//   'databasepass',
-//   {
-//     dialect: 'mysql',
-//     host: 'database-1.cjupvnwhmyug.us-east-2.rds.amazonaws.com',
-//     port: 3306,
-//   },
-// );
 export const db = new Sequelize(
   'gaspar',
-  'root',
-  'Hornet2014@',
+  'admin',
+  'databasepass',
   {
     dialect: 'mysql',
-    host: '127.0.0.1',
+    host: 'database-1.cjupvnwhmyug.us-east-2.rds.amazonaws.com',
     port: 3306,
   },
 );
+// export const db = new Sequelize(
+//   'gaspar',
+//   'root',
+//   'Hornet2014@',
+//   {
+//     dialect: 'mysql',
+//     host: '127.0.0.1',
+//     port: 3306,
+//   },
+// );
